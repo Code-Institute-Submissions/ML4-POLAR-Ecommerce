4605817b19315358012c895142e9b8fb931c18a5
Anthony <anthony.white2001@hotmail.com>
2020-11-23 21:56:44 +0000

    + stripe elements

--- checkout/models.py
+++ checkout/models.py
@@ -4,10 +4,7 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-
 from products.models import Product
-
-# Create your models here.
 
 
 class Order(models.Model):
--- checkout/views.py
+++ checkout/views.py
@@ -7,13 +7,16 @@ from .forms import OrderForm
 def checkout(request):
     cart = request.session.get('cart', {})
     if not cart:
-        messages.error(request, "There's no items in the cart")
+        messages.error(request, "No Item in the Cart")
         return redirect(reverse('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HbrFLHReQY4VHFKhVAabxoF3TvQcohd1zoWXw2YR71t63C0QUy0O1mrEhcekP86elYc5gjMr6f6B2jtDdiYUobO00w8qKCVig',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
+
