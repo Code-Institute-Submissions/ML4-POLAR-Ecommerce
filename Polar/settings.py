ca75caebe9fcafa0e578e62a463d1469fdc5488e
Anthony <anthony.white2001@hotmail.com>
2020-11-29 17:40:05 +0000

    debug set and secret key gone

--- Polar/settings.py
+++ Polar/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^2)=e40ab(d1ynj0vatk82vl7rw*wi4e=$ldnyvv5dok+ogd@g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['polar-ms4-hylia358.herokuapp.com', 'localhost']
 
