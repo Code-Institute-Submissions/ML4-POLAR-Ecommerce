d9804a533c18c58e4f9d7cfa1d47f627a63a1ebd
Anthony <anthony.white2001@hotmail.com>
2020-11-29 17:12:18 +0000

    heroku setting up

--- Polar/settings.py
+++ Polar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '^2)=e40ab(d1ynj0vatk82vl7rw*wi4e=$ldnyvv5dok+ogd@g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polar-ms4-hylia358', 'localhost']
 
 
 # Application definition
@@ -115,12 +115,17 @@ WSGI_APPLICATION = 'Polar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
